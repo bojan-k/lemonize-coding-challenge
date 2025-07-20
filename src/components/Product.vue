126194924b80c46c660c83d3ba9e5357cc8c3d04
Bojan <bojanklecina@web.de>
2025-07-20 16:54:08 +0200

    Removed sample code from App.vue, created Product.vue, ProductList.vue

---
+++ src/components/Product.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    props: {
+        name: String,
+    },
+}
+</script>
+
+<template>
+    <span>{{ this.name }}</span>
+</template>
+
+<style lang="css" scoped>
+@import "tailwindcss";
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,27 @@
+<script>
+import Product from "./Product.vue";
+
+export default {
+    props: {
+        products: Array,
+    },
+    components: {
+        Product,
+    }
+}
+</script>
+
+<template>
+    <div>
+      <Product v-for="product in products" :key="product.id" :name="product.title"/>
+    </div>
+</template>
+
+<style lang="css" scoped>
+@import "tailwindcss";
+
+div {
+    display       : flex;
+    flex-direction: column;
+}
+</style>
