06cfdba10f314a90101fbbcadad522463ab904ea
Bojan <bojanklecina@web.de>
2025-07-24 00:37:57 +0200

    ProductFilterButton: Improved styling.

--- src/components/ProductFilterButton.vue
+++ src/components/ProductFilterButton.vue
@@ -14,7 +14,7 @@ export default {
 </script>
 
 <template>
-    <label class="border border-blue-200 rounded-md px-2 py-1 cursor-pointer select-none text-nowrap" :class='{ "bg-blue-500 hover:bg-blue-600 text-white": this.selected, "bg-white hover:bg-gray-100": !this.selected }'>
+    <label class="border border-blue-200 rounded-md px-2 py-1 cursor-pointer select-none text-blue-500 text-nowrap" :class='{ "bg-blue-500 hover:bg-blue-600 text-white": this.selected, "bg-white hover:bg-blue-100": !this.selected }'>
         <input @change="this.onChange" type="checkbox" class="hidden" :value="this.category.slug" :checked="this.selected"/>
         {{ this.category.name }}
     </label>
