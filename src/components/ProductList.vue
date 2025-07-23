fcab427d64a10996940e964633e2ba419c01995a
Bojan <bojanklecina@web.de>
2025-07-23 21:36:06 +0200

    ProductList: Removed displayCategory.

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,11 +2,6 @@
 import Product from "./Product.vue";
 
 export default {
-    data() {
-        return {
-            displayCategory: null,
-        }
-    },
     props: {
         products: Array,
     },
@@ -19,7 +14,6 @@ export default {
 <template>
     <div class="flex flex-row gap-3 min-w-3/4">
         <div class="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 gap-5">
-            <!-- v-if="!displayCategory || (displayCategory === this.category)" -->
             <Product
                 v-for="product in products" :key="product.id" :name="product.title" :category="product.category" :image="product.thumbnail"
             />
