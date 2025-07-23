f3b0939863156f35748cedbe10617341870c5c35
Bojan <bojanklecina@web.de>
2025-07-23 23:17:53 +0200

    Product: Tweaked price.

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -10,7 +10,7 @@ export default {
     <div class="flex flex-col border border-neutral-200 rounded-lg h-[250px] p-3">
         <span class="text-lg text-nowrap text-ellipsis shrink overflow-hidden">{{ this.product.title }}</span>
         <img v-if="this.product.images.length" :src="this.product.images[0]" class="self-center w-2/3">
-        <i class="text-sm text-right">{{ this.product.price }}</i>
+        <i class="text-sm text-right">{{ this.product.price }} €</i>
     </div>
 </template>
 
